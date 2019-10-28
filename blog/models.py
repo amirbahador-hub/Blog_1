ca02e5ed049c86870a194588c960e4bca6a886ae
ABB <amirbahador.pv@gmail.com>
2019-10-28 05:21:21 -0400

    creating Post model

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Post(models.Model):
+    STATUS_CHOICES = (
+        ('draft', 'Draft'),
+        ('published', 'Published'),
+    )
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=200)
+    body = models.TextField()
+    published = models.DateTimeField(default=timezone.now)
+    created = models.DateTimeField(auto_now_add=True)
+    update = models.DateTimeField(auto_now=True)
+    status = models.CharField(max_length=60, choices=STATUS_CHOICES, default='draft')
