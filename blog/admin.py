dbdc4baeaad5feb903ccbad1a3124a147c63003b
ABB <amirbahador.pv@gmail.com>
2019-10-28 07:11:40 -0400

    creating admin panel superuser and add data with django shell

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Post
 
-# Register your models here.
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'slug', 'published', 'status')
+    list_display_links = ('title',)
+    list_filter = ('published', 'status')
+    search_fields = ('title', 'body')
+    ordering = ('status', 'published')
+    prepopulated_fields = {'slug': ('title',)}
+
+
+
+admin.site.register(Post, PostAdmin)
--- blog/models.py
+++ blog/models.py
@@ -14,3 +14,6 @@ class Post(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     update = models.DateTimeField(auto_now=True)
     status = models.CharField(max_length=60, choices=STATUS_CHOICES, default='draft')
+
+    def __str__(self):
+        return "Post object (id={} & title={})".format(self.id, self.title)
