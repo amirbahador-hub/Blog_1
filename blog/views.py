38d7cab65f98f2b0cdfe7f7932a99644cb965d5f
ABB <amirbahador.pv@gmail.com>
2019-10-29 10:49:25 -0400

    ADD front

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from datetime import datetime
+from .models import Post
+from django.http import HttpResponse
 
-# Create your views here.
+#
+def index(request):
+    return HttpResponse("Hello World!")
+#
+# def single(request):
+#     return render(request, 'single.html', {})
+
+#     latest_post = Post.objects.order_by('-published')[:1]
+#
+#     context = {
+#         'latest_post': latest_post
+#     }
+    # return render(request, 'index.html', context)
